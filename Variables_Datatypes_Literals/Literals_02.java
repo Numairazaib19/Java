4d014ae6eb82c174fbd19e83482f976bd6f0cd1f
Numaira Zaib <57365358+Numairazaib19@users.noreply.github.com>
2023-06-27 17:35:52 +0500

    Add files via upload

---
+++ Variables_Datatypes_Literals/Literals_02.java
@@ -0,0 +1,20 @@
+public class Literals_02 {
+    public static void main(String[] args) {
+        byte b1 = 33;
+        int i1 = 67;
+        short s1 = 5;
+        char ch1 = 'N';
+        float f1 = 23.4f;
+        double d1 = 56.7;
+        long l1 = 67777777777777L;
+        boolean bol1 = true;
+        System.out.println(b1);
+        System.out.println(i1);
+        System.out.println(s1);
+        System.out.println(ch1);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(l1);
+        System.out.println(bol1);
+    }
+}
---
+++ Variables_Datatypes_Literals/Variables_Datatype_01.java
@@ -0,0 +1,16 @@
+// sum of three numbers
+// int = datatype
+// num1, num2, num3 are variables
+
+public class Variables_Datatype_01 {
+    public static void main(String[] args) {
+        System.out.print("Sum of Three numbers is: ");
+        int num1 = 6;
+        int num2 = 2;
+        int num3 = 4;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+
+    }
+}
+
