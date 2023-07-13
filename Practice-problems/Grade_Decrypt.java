44cb575775afcb33da3e0c5cb8a2842c299ccf61
Numaira Zaib <57365358+Numairazaib19@users.noreply.github.com>
2023-07-13 15:23:10 +0500

    Add files via upload

---
+++ Practice-problems/Grade_Decrypt.java
@@ -0,0 +1,10 @@
+public class Grade_Decrypt {
+    public static void main(String[] args) {
+        char grade = 'B';
+        grade = (char)(grade + 8);
+        System.out.println(grade);
+
+        grade = (char)(grade - 8);
+        System.out.println(grade);
+    }
+}
---
+++ Practice-problems/Result_of_given_expression.java
@@ -0,0 +1,13 @@
+// precedence and associativity
+
+public class Result_of_given_expression {
+    public static void main(String[] args) {
+        float a = 7/4*9/2;
+        System.out.println(a);
+
+        // correct way
+        float b = 7/4f*9/2f;
+        System.out.println(b);
+
+    }
+}
